ebbaaf1129b0ec7e3aa782f6c7d99fe08afca543
David Davis <davisd50@gmail.com>
2016-03-20 19:09:40 -0400

    update zcml loaded components

--- sparc/apps/cache/testing.py
+++ sparc/apps/cache/testing.py
@@ -1,15 +1,13 @@
-from zope.component.testlayer import ZCMLLayerBase
+from zope.component.testlayer import ZCMLFileLayer
 import sparc.apps.cache
 from sparc.configuration.zcml.configure import configure_vocabulary_registry
 
-class SparcCacheLayer(ZCMLLayerBase):
+class SparcCacheLayer(ZCMLFileLayer):
     def setUp(self):
         super(SparcCacheLayer, self).setUp()
         configure_vocabulary_registry()
     def tearDown(self):
         super(SparcCacheLayer, self).tearDown()
-    
-    def _load_zcml(self, context):
-        pass # app is responsible to do this
 
-SPARC_CACHE_INTEGRATION_LAYER = SparcCacheLayer(sparc.apps.cache)
+SPARC_CACHE_INTEGRATION_LAYER = SparcCacheLayer(sparc.apps.cache,
+                                                zcml_file="configure.zcml")
